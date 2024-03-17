f6cd5ed80cb3351a57f4cd9e75c1fb72dcc40490
Patrick Monaghan <0x5f.manpat@gmail.com>
2024-03-17 17:05:10 +0000

    Add EntityCollection trait for abstracting over things that can yield EntityRefs

--- src/types.rs
+++ src/types.rs
@@ -201,3 +201,33 @@ impl Deref for EntityRef<'_> {
 
 // TODO: entity queries
 // TODO: mesh building
+
+
+pub trait EntityCollection<'t> {
+	fn into_entities(self) -> impl Iterator<Item=EntityRef<'t>>;
+
+	fn into_entities_with_prefix<'p>(self, prefix: &'p str) -> impl Iterator<Item=EntityRef<'t>> + 'p where Self : Sized, Self: 't, 't: 'p {
+		self.into_entities()
+			.filter(move |entity| entity.name.starts_with(prefix))
+	}
+}
+
+impl<'t> EntityCollection<'t> for &'t Project {
+	fn into_entities(self) -> impl Iterator<Item=EntityRef<'t>> {
+		self.entities()
+	}
+}
+
+impl<'t> EntityCollection<'t> for SceneRef<'t> {
+	fn into_entities(self) -> impl Iterator<Item=EntityRef<'t>> {
+		self.entities()
+	}
+}
+
+impl<'t, T> EntityCollection<'t> for T
+	where T: Iterator<Item=EntityRef<'t>>
+{
+	fn into_entities(self) -> impl Iterator<Item=EntityRef<'t>> {
+		self
+	}
+}
